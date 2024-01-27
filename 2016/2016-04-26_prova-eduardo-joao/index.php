2641a1068bc93b015f08345c3f041f3844488b18
JoaoGH <gabrielhartmannqwert@gmail.com>
2024-01-27 15:27:14 -0300

    feat: adicionar código fonte da prova de PHP

---
+++ 2016/2016-04-26_prova-eduardo-joao/index.php
@@ -0,0 +1,35 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Casa do Celular</title>
+<link href="estilo.css" rel="stylesheet" type="text/css" /> 
+</head>
+<body>
+<div id="container">
+
+ <div id="topo">    </div> <!-- fim da div topo -->
+ <div id="menu">
+    <div id="link1"> <a href="index.php"> Início </a></div>
+    <div id="link2"> <a href="produtos.html"> Produtos </a></div>
+    <div id="link3"> <a href="videos.html"> Vídeos </a></div>
+    <div id="link4"> <a href="contato.php"> Contatos </a></div>  	
+ </div> <!-- fim da div menu --> 
+ <div id="conteudo">
+     <div class="texto">
+         <p>O nosso site visa principalmente as pessoas apaixonadas por celulares e smartphones de ultima geração</p>
+         <p>Aqui temos os principais modelos do momento, os celulares daqui sao completamente originais e sem replicas ou copias. </p>
+         <img class="imagem" src="imagem_1.jpg"/>
+         <p>Vendemos celulares usados e novos, com garantia e qualidade nas vendas</p>
+         <p>Um site muito criativo e simples, mas porem com grande conteúdo</p>    
+         <p>Aqui vendemos celulares nas plataformas IOS, Android</p>
+         <img class="imagem_alingn" src="apresentacao2.jpg"/>
+         <img class="imagem_alingn" src="apresentacao3.jpg"/>
+     </div>
+ </div> <!-- fim da div conteudo -->
+ <div id="rodape"><h3>Eduardo de Cristo Meurer</h3> </div> <!-- fim da div rodape -->
+
+</div> <!-- fim da div container -->
+
+</body>
+</html>
