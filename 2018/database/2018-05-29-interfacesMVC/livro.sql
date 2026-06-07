3c0257177b4990ac78a96fb4bc0ce2507928229f
JoaoGH <gabrielhartmannqwert@gmail.com>
2026-06-07 17:52:30 -0300

    fix: corrigir sql

--- 2018/database/2018-05-29-interfacesMVC/livro.sql
+++ 2018/database/2018-05-29-interfacesMVC/livro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 29-Maio-2018 às 16:29
+-- Generation Time: 30-Maio-2018 às 04:00
 -- Versão do servidor: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -38,22 +38,14 @@ CREATE TABLE IF NOT EXISTS `info` (
   `autor3` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `editora` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `info`
 --
 
 INSERT INTO `info` (`id`, `titulo`, `ano`, `autor`, `autor2`, `autor3`, `editora`) VALUES
-(2, 'titulo', 2018, 'eu', '', '', ''),
-(3, 'titulo', 2018, 'eu', '', '', ''),
-(4, 'titulo', 2018, 'eu', '', '', ''),
-(5, 'titulo', 2018, 'eu', '', '', ''),
-(6, 'titulo2', 2018, 'eu', '', '', ''),
-(7, 'titulo2', 2018, 'eu', '', '', ''),
-(8, 'titulo3', 2019, 'eu', 'tu', 'ele', 'fevereiro'),
-(9, 'titulo3', 2019, 'eu', 'tu', '', 'fevereiro'),
-(10, 'titulo4', 2019, 'aquela cara lÃ¡', '', '', 'marÃ§o');
+(11, 'titulo', 2018, 'autor', '', '', 'Abril');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
